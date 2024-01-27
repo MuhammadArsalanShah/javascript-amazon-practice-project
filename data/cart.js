4708d3059d5dcf0bc9c71a653e1a56b65263e939
ArsalanShah <arsalanshah408@yahoo.com>
2024-01-27 12:16:42 +0500

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,12 +3,14 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
   cart = [
     {
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 4
+      productId: 'myId-001',
+      quantity: 4,
+      deliveryOptionId: '1'
     },
     {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 2
+      productId: 'myId-002',
+      quantity: 2,
+      deliveryOptionId: '2'
     }
   ];
 }
@@ -37,7 +39,7 @@ export function addToCart(productId) {
   if (matchingItem) {
     matchingItem.quantity += quantity;
   } else {
-    cart.push({ productId, quantity });
+    cart.push({ productId, quantity, deliveryOptionId: '1' });
   }
 
   saveToStorage();
