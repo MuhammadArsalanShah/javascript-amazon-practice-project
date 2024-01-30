b215675fcf96ffb51567b84b932ccf5c18830624
ArsalanShah <arsalanshah408@yahoo.com>
2024-01-30 11:57:14 +0500

    Practice Exercises 15

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,14 +3,11 @@ updateDeliveryOption } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary() {
-  
-  /** Updading Cart item header **/
-  updateCartQuantity();
-
   let cartSummaryHTML = '';
 
   cart.forEach((cartItem) => {
@@ -22,12 +19,7 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId)
 
-    const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
-    const dateString = deliveryDate.format('dddd, MMMM D');
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
       <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -76,12 +68,8 @@ export function renderOrderSummary() {
     let html = '';
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays,
-        'days'
-      );
-      const dateString = deliveryDate.format('dddd, MMMM D');
+      
+      const dateString = calculateDeliveryDate(deliveryOption);
 
       const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} - `;
 
@@ -116,10 +104,8 @@ export function renderOrderSummary() {
       const productId = deleteLink.dataset.productId;
       removeFromCart(productId);
 
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();
-
-      updateCartQuantity();
+      renderCheckoutHeader();
+      renderOrderSummary();
       renderPaymentSummary();
     });
   });
@@ -182,18 +168,9 @@ function quantityUpdationHandle(productId) {
   container.classList.remove('is-editing-quantity');
 
   updateQuantity(productId, newQuantity);
-    
-  const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
-  quantityLabel.innerHTML = newQuantity;
 
-  updateCartQuantity();
+  renderCheckoutHeader();
+  renderOrderSummary();
   renderPaymentSummary();
 }
 /********** Quantity Updation Handle function end **********/
-
-/** Update cart quantity function start (will not conflict with same function in amazon.js) **/
-function updateCartQuantity() {
-  const cartQuantity = calculateCartQuantity();
-  document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
-}
-/************ Update cart quantity function end ************/
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import { cart } from "../../data/cart.js";
+import { cart, calculateCartQuantity } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
@@ -27,7 +27,7 @@ export function renderPaymentSummary() {
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items (${calculateCartQuantity()}):</div>
       <div class="payment-summary-money">
        $${formatCurrency(productPriceCents)}
       </div>
@@ -69,3 +69,5 @@ export function renderPaymentSummary() {
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
 }
+
+
