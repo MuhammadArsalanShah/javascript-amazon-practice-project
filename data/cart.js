9825c9601828d9e6e4152fabeefc4466ef27d40d
ArsalanShah <arsalanshah408@yahoo.com>
2024-07-08 11:37:29 +0500

    Learn Promises & converting the code to Promises

--- data/cart.js
+++ data/cart.js
@@ -103,3 +103,22 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   saveToStorage();
 }
 /************ Update delivery option function end ************/
+
+
+
+
+
+export function loadCart(fun) {
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    
+    console.log(xhr.response);
+    fun();
+  
+  });
+  
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,69 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/car.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+// Promises don't nest the code
+// and keep it flat and easy to understand
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('my value to resolve');
+    });
+  
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+
+  console.log(values);
+
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+})
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('my value to resolve');
+  });
+
+}).then((value) => {
+
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
+
+/*
+
+// call backs causes too musch nesting
+// and makes the code difficult to understand
+
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
+
